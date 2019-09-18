d474ce56f034f961c2a9ea115d6851e9e51cc09f
qiwang <qiwa3476@colorado.edu>
2019-09-18 14:39:34 -0600

    updated

--- test.cpp
+++ test.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-void add(int &a, int &b)
-{
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
 int main()
 {
-    int a = 3;
-    int b = 4;
-    add(a, b);
-    std::cout << a << " " << b << std::endl;
+    int a[] = {1, 2, 3, 4, 5};
+    for(int i = 0; i < 5; i ++){
+      std::cout << *(a + i) << std::endl;
+    }
     return 0;
 }
